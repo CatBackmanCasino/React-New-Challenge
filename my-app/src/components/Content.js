4693371c3ecc971619864ca398580af5766ae369
CatBackmanCasino <eric.aj.backman@gmail.com>
2023-01-05 17:12:36 +0000

    content and loader.js using setTimeout and componentDidMount

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css"
 import {savedPosts} from "../posts.json"
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
+            isLoaded: false
         }
     }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 1000)
+    }
+
     render() {
         return (
             <div>
@@ -17,16 +28,24 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    {
-                        savedPosts.map(post => {
-                            return (
-                                <PostItem 
-                                key={post.title}
-                                post={post}/>
-                            )
-                        })
+                  {this.state.isLoaded ? (
+                                          savedPosts.map(post => {
+                                            return (
+                                                <PostItem 
+                                                key={post.title}
+                                                post={post}/>
+                                            )
+                                        })
+                  ) : (
+                    <Loader />
+                  )}
+                  {
+
                     }
+
+
                 </div>
+
             </div>
         )
     }
